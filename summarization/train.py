777e24d72e0d472ec315cafb61399bf76c9a5c0c
davgur <davgur.ru@gmail.com>
2024-08-04 15:04:46 +0300

    fix 9

--- summarization/train.py
+++ summarization/train.py
@@ -6,6 +6,7 @@ import json
 
 import nltk
 import torch
+from adapters import LoRAConfig, AdapterTrainer
 
 from datasets import load_dataset, concatenate_datasets, DatasetDict, Dataset
 from transformers.utils.quantization_config import QuantizationMethod
@@ -24,7 +25,8 @@ with open('models/dataset.txt') as f:
 # ds = load_dataset("billsum", split="ca_test")
 # ds = ds.train_test_split(test_size=0.2)
 
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, BitsAndBytesConfig, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, BitsAndBytesConfig, AutoModelForCausalLM, \
+    TrainingArguments
 
 model_checkpoint = "google/flan-t5-small"
 # model_checkpoint = "./models/mt5-small"
@@ -32,33 +34,41 @@ tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, load_in_8bit=True, d
 
 from peft import LoraConfig, get_peft_model, TaskType, prepare_model_for_kbit_training
 
-lora_config = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    target_modules=["q", "v"],
-    lora_dropout=0.05,
-    bias="none",
-    task_type=TaskType.SEQ_2_SEQ_LM,
-)
-q_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_compute_dtype=torch.bfloat16,
-    quant_method=QuantizationMethod.BITS_AND_BYTES,
-    lora_config=lora_config
-)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint, load_in_8bit=True, device_map="auto")
-model = prepare_model_for_kbit_training(model)
-model = get_peft_model(model, lora_config)
-model.print_trainable_parameters()
-
+# lora_config = LoraConfig(
+#     r=16,
+#     lora_alpha=32,
+#     target_modules=["q", "v"],
+#     lora_dropout=0.05,
+#     bias="none",
+#     task_type=TaskType.SEQ_2_SEQ_LM,
+# )
+# q_config = BitsAndBytesConfig(
+#     load_in_4bit=True,
+#     bnb_4bit_quant_type="nf4",
+#     bnb_4bit_use_double_quant=True,
+#     bnb_4bit_compute_dtype=torch.bfloat16,
+#     quant_method=QuantizationMethod.BITS_AND_BYTES,
+#     lora_config=lora_config
+# )
 # model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint, load_in_8bit=True, device_map="auto")
-# model = prepare_model_for_int8_training(model)
-# model = get_peft_model(model, config)
-# model.print_trainable_parameters()
-# model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint, load_in_8bit=True, device_map="auto")
+# model = prepare_model_for_kbit_training(model)
 # model = get_peft_model(model, lora_config)
+# model.print_trainable_parameters()
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint, load_in_8bit=True, device_map="auto")
+config = LoRAConfig(
+    r=8,
+    alpha=16,
+    # use it on all of the layers
+    intermediate_lora=True,
+    output_lora=True
+)
+
+# make a new adapter for the XSum dataset
+model.add_adapter("xsum", config=config)
+# enable the adapter for training
+model.train_adapter("xsum")
+model.set_active_adapters(["xsum"])
 
 from transformers import Seq2SeqTrainingArguments
 
@@ -138,27 +148,50 @@ data_collator(features)
 
 from transformers import Seq2SeqTrainer
 
-args = Seq2SeqTrainingArguments(
-    output_dir="summ_he",
-    auto_find_batch_size=True,
-    learning_rate=2e-5,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    weight_decay=0.01,
-    save_total_limit=3,
-    num_train_epochs=4,
-    predict_with_generate=True,
-    fp16=True,
+# args = Seq2SeqTrainingArguments(
+#     output_dir="summ_he",
+#     auto_find_batch_size=True,
+#     learning_rate=2e-5,
+#     per_device_train_batch_size=16,
+#     per_device_eval_batch_size=16,
+#     weight_decay=0.01,
+#     save_total_limit=3,
+#     num_train_epochs=4,
+#     predict_with_generate=True,
+#     fp16=True,
+# )
+
+# trainer = Seq2SeqTrainer(
+#     model,
+#     args,
+#     train_dataset=tokenized_datasets["train"],
+#     eval_dataset=tokenized_datasets["test"],
+#     data_collator=data_collator,
+#     tokenizer=tokenizer,
+#     compute_metrics=compute_metrics,
+# )
+
+# small batch size to fit in memory
+batch_size = 1
+
+training_args = TrainingArguments(
+    learning_rate=3e-4,
+    num_train_epochs=1,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    logging_steps=200,
+    output_dir="./training_output",
+    overwrite_output_dir=True,
+    remove_unused_columns=False
 )
 
-trainer = Seq2SeqTrainer(
-    model,
-    args,
+# create the trainer
+trainer = AdapterTrainer(
+    model=model,
+    args=training_args,
+    tokenizer=tokenizer,
     train_dataset=tokenized_datasets["train"],
     eval_dataset=tokenized_datasets["test"],
-    data_collator=data_collator,
-    tokenizer=tokenizer,
-    compute_metrics=compute_metrics,
 )
 model.config.use_cache = False
 
