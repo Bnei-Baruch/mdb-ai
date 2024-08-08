d2000fe07581c850434b395e136065f41a64e416
davgur <davgur.ru@gmail.com>
2024-08-08 10:21:14 +0300

    fix

--- summarization/train.py
+++ summarization/train.py
@@ -1,14 +1,14 @@
-# pip install datasets evaluate transformers[sentencepiece]
-# pip install accelerate
-# pip install rouge_score
-# pip install nltk
 import json
 
+import evaluate
 import nltk
+import numpy as np
 import torch
 from datasets import Dataset
-
-#
+from nltk.tokenize import sent_tokenize
+from peft import LoraConfig, TaskType, prepare_model_for_kbit_training, get_peft_model
+from transformers import MT5Tokenizer, BitsAndBytesConfig, Seq2SeqTrainingArguments, \
+    Seq2SeqTrainer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq
 
 torch.cuda.empty_cache()
 
@@ -19,21 +19,11 @@ with open('models/dataset.txt') as f:
     ds.set_format(type="torch", columns=["article", "summary"])
     ds = ds.train_test_split(test_size=0.2)
 
-# ds = load_dataset("billsum", split="ca_test")
-# ds = ds.train_test_split(test_size=0.2)
-
-from transformers import AutoTokenizer, BitsAndBytesConfig, AutoModel, Seq2SeqTrainingArguments, \
-    Seq2SeqTrainer, AutoModelForSeq2SeqLM
-
-# model_checkpoint = "google/flan-t5-small"
-#model_checkpoint = "google/mt5-small"
+# model_checkpoint = "google/mt5-small"
 model_checkpoint = "google/mt5-base"
 
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+tokenizer = MT5Tokenizer.from_pretrained(model_checkpoint)
 
-from peft import LoraConfig, TaskType, prepare_model_for_kbit_training, get_peft_model
-
-#
 lora_config = LoraConfig(
     r=16,
     lora_alpha=32,
@@ -49,31 +39,20 @@ q_config = BitsAndBytesConfig(
     bnb_4bit_use_double_quant=True,
     bnb_4bit_compute_dtype=torch.bfloat16
 )
-# model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint, quantization_config=q_config)
-# model = prepare_model_for_kbit_training(model)
-# model = get_peft_model(model, lora_config)
-# model.print_trainable_parameters()
+
+max_input_length = 4096
+max_target_length = 200
+prefix = "summarize: "
+label_pad_token_id = -100
+# small batch size to fit in memory
+batch_size = 1
+num_train_epochs = 8
 
 model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint, quantization_config=q_config)
-
-# config = LoRAConfig(
-#     r=8,
-#     alpha=16,
-#     intermediate_lora=True,
-#     output_lora=True
-# )
-
-# model = AutoPeftModelForSeq2SeqLM(model, config=lora_config)
 model = prepare_model_for_kbit_training(model)
 adapter_name_he = "summ_he"
 model.add_adapter(adapter_name=adapter_name_he, adapter_config=lora_config)
 model = get_peft_model(model.base_model, lora_config)
-# model.active_adapters = adapter_name_he
-
-max_input_length = 4096
-max_target_length = 200
-
-prefix = "summarize: "
 
 
 def preprocess_function(examples):
@@ -86,7 +65,8 @@ def preprocess_function(examples):
     inputs = [prefix + txt for txt in examples["article"]]
     model_inputs = tokenizer(inputs, max_length=max_input_length, truncation=True)
 
-    labels = tokenizer(text_target=examples["summary"], max_length=max_target_length, truncation=True)
+    labels = tokenizer(text_target=examples["summary"], max_length=max_target_length,
+                       max_source_length=max_target_length, truncation=True)
 
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
@@ -94,18 +74,10 @@ def preprocess_function(examples):
 
 tokenized_datasets = ds.map(preprocess_function, batched=True)
 print("tokenize of dataset was ended")
-import evaluate
 
 rouge_score = evaluate.load("rouge")
-
-batch_size = 8
-num_train_epochs = 8
 # Show the training loss with every epoch
 logging_steps = len(tokenized_datasets["train"]) // batch_size
-
-import numpy as np
-from nltk.tokenize import sent_tokenize
-
 nltk.download('punkt')
 
 
@@ -119,16 +91,15 @@ def compute_metrics(eval_pred):
     decoded_labels = ["\n".join(sent_tokenize(label.strip())) for label in decoded_labels]
 
     result = rouge_score.compute(
-        predictions=decoded_preds, references=decoded_labels, use_stemmer=True
+        predictions=decoded_preds,
+        references=decoded_labels,
+        use_stemmer=True
     )
+    print(list(result.keys()))
     result = {key: value * 100 for key, value in result.items()}
     return {k: round(v, 4) for k, v in result.items()}
 
 
-# from transformers import DataCollatorForSeq2Seq
-from transformers import DataCollatorForSeq2Seq
-
-label_pad_token_id = -100
 data_collator = DataCollatorForSeq2Seq(
     tokenizer,
     model=model,
@@ -141,32 +112,6 @@ tokenized_datasets = tokenized_datasets.remove_columns(ds["train"].column_names)
 features = [tokenized_datasets["train"][i] for i in range(2)]
 data_collator(features)
 
-# args = Seq2SeqTrainingArguments(
-#     output_dir="summ_he",
-#     auto_find_batch_size=True,
-#     learning_rate=2e-5,
-#     per_device_train_batch_size=16,
-#     per_device_eval_batch_size=16,
-#     weight_decay=0.01,
-#     save_total_limit=3,
-#     num_train_epochs=4,
-#     predict_with_generate=True,
-#     fp16=True,
-# )
-
-# trainer = Seq2SeqTrainer(
-#     model,
-#     args,
-#     train_dataset=tokenized_datasets["train"],
-#     eval_dataset=tokenized_datasets["test"],
-#     data_collator=data_collator,
-#     tokenizer=tokenizer,
-#     compute_metrics=compute_metrics,
-# )
-
-# small batch size to fit in memory
-batch_size = 1
-
 training_args = Seq2SeqTrainingArguments(
     learning_rate=3e-4,
     num_train_epochs=1,
@@ -178,9 +123,6 @@ training_args = Seq2SeqTrainingArguments(
     remove_unused_columns=False,
 )
 
-# for param in model.parameters():
-#     param.requires_grad = False
-
 trainer = Seq2SeqTrainer(
     model=model,
     args=training_args,
@@ -190,10 +132,9 @@ trainer = Seq2SeqTrainer(
     compute_metrics=compute_metrics,
 )
 
-# model.train_adapter(adapter_name_he)
 model.config.use_cache = False
+model.config.max_position_embeddings = max_input_length
 
-print("start training...", model)
+print("start training")
 trainer.train()
-print("start evaluating...")
-#trainer.evaluate()
+print("end of training", model)
--- summarization/use_model.py
+++ summarization/use_model.py
@@ -13,11 +13,11 @@ def run_summarization(t):
     model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint_fine_tuned)
     model_2 = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
 
-    prefix = "summarize:"
+    prefix = "summarize: "
     input_text = f"{prefix} {t}"
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
-    outputs = model.generate(input_ids)
+    outputs = model.generate(input_ids, num_beams=4, no_repeat_ngram_size=2, min_length=100, max_length=300)
     print(tokenizer.decode(outputs[0]))
     print(tokenizer.decode(model_2.generate(input_ids)[0]))
 
