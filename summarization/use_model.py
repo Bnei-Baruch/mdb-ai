8851b0bffedebe46590b90ff753ac0e0bfa52a7b
davgur <davgur.ru@gmail.com>
2024-08-08 12:57:34 +0300

    fix

--- summarization/use_model.py
+++ summarization/use_model.py
@@ -1,4 +1,3 @@
-import os
 from urllib.request import Request, urlopen
 
 from bs4 import BeautifulSoup
@@ -11,15 +10,15 @@ model_checkpoint = "google/mt5-base"
 def run_summarization(t):
     tokenizer = MT5Tokenizer.from_pretrained(model_checkpoint_fine_tuned, max_seq_len=4096)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint_fine_tuned)
-    model_2 = AutoModelForSeq2SeqLM.from_pretrained(model_checkpoint)
 
     prefix = "summarize:"
     input_text = f"{prefix} {t}"
     input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
     outputs = model.generate(input_ids, num_beams=4, no_repeat_ngram_size=2, min_length=100, max_length=300)
-    print(tokenizer.decode(outputs[0]))
-    print(tokenizer.decode(model_2.generate(input_ids)[0]))
+    res = tokenizer.decode(outputs[0])
+    print(res)
+    return res
 
 
 if __name__ == '__main__':
